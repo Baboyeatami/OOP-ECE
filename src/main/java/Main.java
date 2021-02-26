4e049c24d8957f84364b272e393b629883c6e5ae
Jamie <Jamie@DESKTOP-RCLMTNF>
2021-02-26 09:06:32 +0800

    version 1

---
+++ src/main/java/Main.java
@@ -0,0 +1,46 @@
+
+import java.util.Scanner;
+
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+/**
+ *
+ * @author Jamie Eduardo Rosal <Jamiewertalmighty@gmail.com>
+ */
+public class Main {
+
+    public static void main(String[] args) {
+
+        Scanner input = new Scanner(System.in);
+
+        System.out.println("Enter a value ");
+
+        int a = input.nextInt();
+
+        switch (a) {
+
+            case 1:
+                System.out.println("waaw ang galing ");
+
+                if (a == 1) {
+                    System.out.println("OMG ang galing talagas");
+                }
+
+                break;
+
+            case 2:
+                System.out.println("oh no! galing parin");
+                break;
+
+            default:
+
+                System.out.println("wala sa choises ");
+                System.out.println("hahah");
+
+        }
+
+    }
+}
