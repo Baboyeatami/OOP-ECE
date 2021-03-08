6c62505408812bb27bf83549bd0015bd82410e5f
Jamie <Jamie@DESKTOP-RCLMTNF>
2021-03-08 19:46:55 +0800

    version 4 files handling

--- src/lecture/Lecture.java
+++ src/lecture/Lecture.java
@@ -23,9 +23,49 @@ public class Lecture {
      */
     public static void main(String[] args) {
 
-        Frame1 main = new Frame1();
+        try {
+            File file = new File("filename.txt");
+            FileWriter myWriter = new FileWriter(file, true);
+            BufferedWriter br = new BufferedWriter(myWriter);
 
-        main.setVisible(true);
+            if (file.length() == 0) {
+                String input = JOptionPane.showInputDialog("Enter input");
+
+                br.write(input);
+
+                br.close();
+                myWriter.close();
+
+                myWriter.close();
+            } else {
+                String input = JOptionPane.showInputDialog("Enter input");
+                br.newLine();
+                br.append(input);
+
+                br.close();
+                myWriter.close();
+
+                myWriter.close();
+            }
+
+            int sum = 0;
+
+            Scanner myReader = new Scanner(file);
+
+            while (myReader.hasNextLine()) {
+                String data = myReader.nextLine();
+                System.out.println(data);
+
+                sum = sum + Integer.parseInt(data);
+
+            }
+            myReader.close();
+
+            System.out.println("sum id : " + sum);
+
+        } catch (IOException ex) {
+            Logger.getLogger(Lecture.class.getName()).log(Level.SEVERE, null, ex);
+        }
 
     }
 
--- src/lecture/Main_jamie.java
+++ src/lecture/Main_jamie.java
@@ -1,6 +1,5 @@
 package lecture;
 
-
 import java.util.Scanner;
 
 /*
@@ -17,32 +16,29 @@ public class Main_jamie {
     public static void main(String[] args) {
 
         Scanner input = new Scanner(System.in);
+        int a = 0, b = 0;
 
-        System.out.println("Enter a value ");
+        System.out.println("Enter a");
+        a = input.nextInt();
 
-        int a = input.nextInt();
-        int x = input.nextInt();
+        while (a == 1) {
+            System.out.println("Welcome to b");
+            b = input.nextInt();
 
-        switch (a) {
-
-            case 1:
-                System.out.println("waaw ang galing ");
-
-                if (a == 1) {
-                    System.out.println("OMG ang galing talagas");
-                }
-
+            if (b == 2) {
                 break;
+            } else if (b == 3) {
+                System.out.println("welcome to looper ");
+                System.out.println("Enter loop limit");
 
-            case 2:
-                System.out.println("oh no! galing parin");
-                System.out.println("");
-                break;
+                int x = 0, limit;
+                limit = input.nextInt();
+                do {
+                    System.out.println(x);
+                    x++;
+                } while (x <= limit);
 
-            default:
-
-                System.out.println("wala sa choises ");
-                System.out.println("hahah");
+            }
 
         }
 
